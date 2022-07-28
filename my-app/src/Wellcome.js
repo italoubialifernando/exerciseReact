b09be5b0ce8e41586d215b84acab0e65f292e0c9
italo fernando ubiali <you@example.com>
2022-07-28 10:09:03 +0200

    con-rend-4

--- my-app/src/Wellcome.js
+++ my-app/src/Wellcome.js
@@ -3,12 +3,14 @@ import React from 'react'
 export class Wellcome extends React.Component {
     render() {
         return <div>
-
             {!!this.props.name && <p>hellow, {this.props.name}!</p>}
             {!this.props.name && <h1>hellow, nobody !</h1>}
-            {this.props.age > 18 && 
+            {this.props.name =="john"?
+            this.props.age > 18 && 
             this.props.age <65 && 
-            <p>your age is? {this.props.age}</p>}
+            <p>your age is? {this.props.age}</p>
+            :
+            <p>jhon?</p> }
         </div>
     }
 }
