c75e2bbff6a543bf8b3b5833f3b0a569da2037b4
italo fernando ubiali <you@example.com>
2022-07-28 09:43:32 +0200

    props5

--- my-app/src/Wellcome.js
+++ my-app/src/Wellcome.js
@@ -5,7 +5,7 @@ export class Wellcome extends React.Component {
         return <div>
             {!!this.props.name && <p>hellow, {this.props.name}!</p>}
             {!this.props.name && <h1>hellow, nobody !</h1>}
-            <p>your age is: {this.props.age}</p>
+            <p>your age is? {this.props.age}</p>
         </div>
     }
 }
