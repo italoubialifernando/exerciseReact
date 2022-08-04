6226989d813e216791664346657113b5f913348f
italo fernando ubiali <you@example.com>
2022-08-04 09:47:03 +0200

    render props01

--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -1,54 +1,56 @@
 import React from "react";
 
 
+
 let list = ["buy milk", "buy another milk", "drink more milk"]
 
-
-
 export class TodoList extends React.Component {
-    state ={
-        addListItem : ''
+    state = {
+        addListItem: '',
+        list : list
     }
 
-    handleInputChange =  (event) => {
-       const value = event.target.value 
+
+    handleInputChange = (event) => {
+        const value = event.target.value
         this.setState({
-            addListItem : value
+            addListItem: value
         })
     }
 
-    HandleItemList =()=>{
+    HandleItemList = () => {
         list.push(this.state.addListItem)
-        this.forceUpdate()
         this.setState({
-            addListItem : ''
+            addListItem: '',
+            list:list
         })
+        this.forceUpdate()
+        console.log(list, this.state.list)
     }
-    HandleClearList =() => {
-        list = []
+    HandleClearList = () => {
+        list=[]
+        this.setState({
+            list:[]
+        })
         this.forceUpdate()
     }
 
-    HandleRemove = (e)=>{
+    HandleRemove = (e) => {
         ((e.target).parentNode).remove()
     }
     
 
 
-
     render() {
-        
-
-        return(
+        return (
             <div>
                 <ul>
-                {list.map((name, index) => (
-                <li key={name + index}>{name}<button onClick={this.HandleRemove}>remove</button></li>))}
+                    {this.props.render(this.state.list)} 
                 </ul>
-                <input
+               <input
                     name="addListItem,"
-                    value ={this.state.addListItem}
-                    onChange ={this.handleInputChange}
+                    value={this.state.addListItem}
+                    onChange={this.handleInputChange}
                 />
                 <button on onClick={this.HandleItemList}>add list item</button>
                 <button on onClick={this.HandleClearList}>Clear list</button>
